d737887276439ab576ea4319fe18d48c9b59393a
Tanvir <tanvirlaravel@gmail.com>
2024-09-21 14:45:34 +0600

    header and footer added statically

---
+++ footer.php
@@ -0,0 +1,114 @@
+   <!-- Footer area start -->
+   <section class="tp-footer-area tp-footer-4-area  p-relative">
+        <div class="tp-footer-4-bg-shape">
+            <img src="assets/img/footer/footer-4-man-shape.png" alt="">
+        </div>
+        <div class="tp-footer-top pt-120 pb-55">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 mb-50">
+                        <div class="tp-footer-widget footer-cols-1 pr-75 tp-footer-widget-cutm-pdg-4 wow fadeInUp" data-wow-delay=".3s" data-wow-duration="1s">
+                            <div class="tp-footer-logo">
+                                <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                            </div>
+                            <div class="tp-footer-text tp-footer-4-text">
+                                <p>Get Special Offers. We Never Spam.</p>
+                            </div>
+                            <div class="tp-footer-widget-content-input tp-footer-4-input mb-5">
+                                <form class="tp-footer-form" action="index.html">
+                                    <input type="email" placeholder="Your Email">
+                                    <span class="tp-footer-email tp-footer-4-email"><i class="fal fa-envelope"></i></span>
+                                    <button type="submit"><span class="tp-footer-4-email-btn"><i class="fa-solid fa-arrow-up-right"></i></span></button>
+                                </form>
+                            </div>
+                            <div class="tp-footer-social tp-footer-4-social">
+                                <a href="#"><i class="flaticon-facebook-app-symbol"></i></a>
+                                <a href="#"><i class="flaticon-instagram"></i></a>
+                                <a href="#"><i class="flaticon-pinterest-logo"></i></a>
+                                <a href="#"><i class="flaticon-social"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-md-6 col-sm-6 mb-50 ">
+                        <div class="tp-footer-widget tp-footer-4-widget footer-cols-2 wow fadeInUp" data-wow-delay=".5s" data-wow-duration="1s">
+                            <h4 class="tp-footer-title tp-footer-4-title">Information</h4>
+                            <div class="tp-footer-list">
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Pricing Plan</a></li>
+                                    <li><a href="#">About US</a></li>
+                                    <li><a href="#">Our Services</a></li>
+                                    <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-50">
+                        <div class="tp-footer-widget tp-footer-4-widget footer-cols-3 pl-50 tp-footer-widget-cutm-pdg-3 wow fadeInUp" data-wow-delay=".7s" data-wow-duration="1s">
+                            <h4 class="tp-footer-title tp-footer-4-title">Support</h4>
+                            <div class="tp-footer-list">
+                                <ul>
+                                    <li><a href="#">Terms of Use</a></li>
+                                    <li><a href="#">Tearms & Condition</a></li>
+                                    <li><a href="#">Help Center</a></li>
+                                    <li><a href="#">Company Privacy</a></li>
+                                    <li><a href="#">Support</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-50">
+                        <div class="tp-footer-widget footer-cols-4 wow fadeInUp" data-wow-delay=".9s" data-wow-duration="1s">
+                            <h4 class="tp-footer-title tp-footer-4-title">Contact</h4>
+                            <div class="tp-footer-thumb-wrap">
+                                <div class="tp-footer-list tp-footer-4-list">
+                                    <ul>
+                                        <li><a href="tel:999212354685894"><i class="flaticon-phone-call"></i> + 999 2123 5468 5894</a></li>
+                                        <li><a href="mailto:techubinfo@mail.com"><i class="fal fa-envelope"></i> techubinfo@mail.com</a></li>
+                                        <li><a href="https://www.google.com/maps/@41.6758525,-86.2531698,18.17z"><i class="fa-light fa-location-dot"></i> <span>Alpes suizos, Airolo, CH, Kammel <br>
+                                            strasse 98 3123 Belp</span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tp-footer-bottom tp-footer-4-bottom pt-30 pb-30">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="tp-footer-copy-right text-center wow fadeInUp">
+                            <p class="tp-footer-copy-paragraph tp-footer-4-copy-paragraph">Full Copyright & Design By <a href="#">@ Theme pure</a> – 2024</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer area end -->
+
+
+    
+    <!-- JS here -->
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/waypoints.js"></script>
+    <script src="assets/js/bootstrap-bundle.js"></script>
+    <script src="assets/js/meanmenu.js"></script>
+    <script src="assets/js/swiper-bundle.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/range-slider.js"></script>
+    <script src="assets/js/magnific-popup.js"></script>
+    <script src="assets/js/nice-select.js"></script>
+    <script src="assets/js/purecounter.js"></script>
+    <script src="assets/js/countdown.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/isotope-pkgd.js"></script>
+    <script src="assets/js/imagesloaded-pkgd.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
